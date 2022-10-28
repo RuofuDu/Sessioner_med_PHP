1e96472a664062ccdb0022a545473881df592134
Ruofu Du <ruofu.du@skola.taby.se>
2022-10-28 13:05:05 +0200

    change

--- formular.php
+++ formular.php
@@ -9,22 +9,26 @@
 </head>
 
 <body>
-    <?php
-    // Start the session
-    session_start();
-    ?>
-    <!DOCTYPE html>
-    <html>
-
-    <body>
-
+    <form action="skript.php" method="post">
         <?php
-        // Set session variables
-        $_SESSION["favcolor"] = "green";
-        $_SESSION["favanimal"] = "cat";
-        echo "Session variables are set.";
+        session_start();
         ?>
+        <!DOCTYPE html>
+        <html>
 
-    </body>
+        <body>
 
-    </html>
+            <?php
+            // remove all session variables
+            session_unset();
+
+            // destroy the session
+            session_destroy();
+
+            ?>
+
+
+
+        </body>
+
+        </html>
